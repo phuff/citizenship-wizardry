00633f78353655fe2287a4aeae962eb118754809
Paul Huff <paul.huff@gmail.com>
2017-04-04 10:28:38 -0600

    Setup multi-questions + new test.

--- questions.js
+++ questions.js
@@ -1,5 +1,95 @@
 var flashcards = 
-    {
+    {"Test 9":
+[{"question": "Name the amendments that guarantee or address voting rights",
+  "answer": {"type": "multi-part",
+             "answers": ["15", "19", "24"]}},
+ {"question":  "Who is the Chief Justice of the Supreme Court now?",
+  "answer": "John Roberts"},
+ {"question": "Name one right only for United States Citizens",
+  "answer": {"type": "subset",
+             "size": 1,
+             "answers": ["Vote in a federal election", "Run for federal office"]}},
+ {"question": "Under our Constitution some powers belong to the Federal Government.  What is one power of the federal government?",
+  "answer": {"type": "subset",
+             "size": 1,
+             "answers": ["To print money", 
+                         "To declare wars", 
+                         "To create an army",
+                         "To make treaties"]}},
+{"question": "Under our Constitution some powers belong to the states.  What is one power of the states?",
+"answer": {"type": "subset",
+          "size": 1,
+          "answers": ["provide schooling and education",
+                     "Provide protection (police)",
+                     "Provide safety (fire department)",
+                     "give a drivers license",
+                     "approve zoning and land use"]}},
+{"question": "What is the head executive of a city government called?",
+"answer": "Mayor"},
+{"question": "Who do we show loyalty to when we say the Pledge of Allegiance?",
+ "answer": {"type": "multi-part",
+           "answers": ["The United States",
+                      "The flag"]}},
+{"question": "What is the head executive of a state government called?",
+"answer": "Governor"},
+{"question": "Who is the Governor of your state now?",
+"answer": "Gary Herbert"},
+{
+    "question": "What are two rights of everyone living in the United States?",
+    "answer": {"type": "subset",
+               "size": 2,
+               "answers": ["freedom of expression", 
+                           "freedom of speech",
+                          "freedom of assembly",
+                          "freedom of worship",
+                          "to petition the government",
+                          "the right to bear arms"]}
+},
+{
+    "question": "What is one responsibility that is only for United States Citizens?",
+    "answer": {"type": "subset",
+               "size": 1,
+               "answers": ["serve on a jury",
+                          "vote in a federal election"]
+}},
+{
+    "question": "What is the capital of your state?",
+    "answer": "Salt Lake"
+},
+{
+  "question": "What are the two major political parties in the United States?",
+  "answer": {"type": "multi-part",
+             "answers": ["Democratic",
+                        "Republican"]
+             }
+},
+{
+    "question": "What is the political party of the President now?",
+    "answer": "Republican"
+},
+{
+    "question": "There are four amendments to the Constitution about who can vote.  Describe one of them?",
+    "answer": {"type": "subset",
+               "size": 1,
+               "answers": ["Citizens 18 and older can vote",
+                          "Any citizen (male or female) can vote",
+                          "You don't have to pay to vote",
+                          "Any citizen of any race can vote"]}
+},
+{
+    "question": "Name one benefit of being a citizen of the United States",
+    "answer": {"type": "subset",
+               "size": 1,
+               "answers": ["Obtain federal government jobs",
+                           "travel with a U.S. Passport",
+                           "petition for close relatives to come to the U.S."]}
+},
+{
+   "question": "What is the name of the Speaker of the House?",
+    "answer": "Paul Ryan"
+}
+],
+     
         "Quiz 14":
         [{"question": "There are 4 amendments to the Constitution about who can vote.  Name one of them.",
          "answer": "Citizens 18 and older can vote.\nAny citizen (male or female) can vote.\nYou don't have to pay to vote.\nA citizen of any race may vote."},
